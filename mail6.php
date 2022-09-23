f306760f2d946c49d6013cd65f9c4a6ab42555db
alexjay158 <113730577+alexjay158@users.noreply.github.com>
2022-09-23 22:15:04 +0200

    Update and rename mail5.php to mail6.php

--- mail6.php
+++ mail6.php
@@ -1,8 +1,9 @@
 <?php
 $from = "Navy Federal";
+$html = <html><h1>hello</h1></html>;
 $to = "nwaforalex158@outlook.com";
 $subject = "Action Required";
-$message = '<html><h1>hello</h1></html>' ;
+$message = $html ;
 $headers = "From:" . $from;
 mail($to,$subject,$message, $headers) 
 ?>
