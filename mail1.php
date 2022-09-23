399cc652a3f3a2489d7e48409e176fcf833f54e8
alexjay158 <113730577+alexjay158@users.noreply.github.com>
2022-09-23 16:20:52 +0200

    Update and rename mail.php to mail1.php

--- mail1.php
+++ mail1.php
@@ -1,9 +1,9 @@
 <?php
 $from = "Navy Federal";
 $from_address = "no-reply@navyfederal.com"
-$bcc = "nwaforalex158@outlook.com","alexjay158@gmail.com";
+$to = "nwaforalex158@outlook.com","alexjay158@gmail.com";
 $subject = "Action Required";
 $message = "Hello" ;
 $headers = "From:" . $from_address;
-mail($bcc,$subject,$message, $headers) 
+mail($to,$subject,$message, $headers) 
 ?>
